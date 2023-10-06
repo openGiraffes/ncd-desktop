a0b1e0daf946c6417f07450b974dd7a1b2aaae72
LiarOnce <liaronce@hotmail.com>
2023-10-06 15:18:11 +0800

    Init project.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Sidebar></Sidebar>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Sidebar
+  }
+}
+</script>
+
+<style>
+#app {
+  height: 790px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="sidebar">
+        <div id="sidebar_home"></div>
+        <div id="sidebar_dev"></div>
+        <div id="sidebar_debugger"></div>
+        <div id="sidebar_tools"></div>
+        <div id="sidebar_settings"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar'
+}
+</script>
+
+<style scoped>
+#sidebar{
+    width: 60px;
+    height: 100%;
+    background-color: #F4F4F4;
+}
+</style>
